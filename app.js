a5f9f7ad85a994fc9e590709bfe0b6f4770704dc
Brian Leung <brianleung329@gmail.com>
2024-07-22 15:50:01 -0400

    Misc data cleaning and testing.

--- app.js
+++ app.js
@@ -3,28 +3,27 @@ const data = [10,20,30,40,50];
 const svg = d3
   .select("#dvz")
   .append("svg")
-  .attr("width", 800)
-  .attr("height", 400);
+  .attr("width", 1200)
+  .attr("height", 500);
 
 console.log("test")
 
-all_data = d3.csv("https://raw.githubusercontent.com/bleung329/cs416_final_project/main/data/Transistor_count_2.csv").then(function(data) {
+const all_data = d3.csv("https://raw.githubusercontent.com/bleung329/cs416_final_project/main/data/processors.csv").then(function(data) {
   console.log(data);
-})
-
-console.log(all_data.transistor_count)
-
-svg
+  svg
   .selectAll("rect")
   .data(data)
   .enter()
   .append("rect")
-  .attr("x", (d, i) => i * 100 + 50)
-  .attr("y", (d) => 400 - d)
+  .attr("x", function(d, i){console.log(i+50); return i + 50;})
+  .attr("y", function(d,i){return 300;})
   .attr("width", 50)
-  .attr("height", (d) => d)
+  .attr("height", function(d,i){ console.log(d.transistor_count); return 0.00001*Number(d.transistor_count.replace(/,/g, ''));})
   .attr("fill", "red");
+  console.log("DONE?")
+})
 
+console.log(all_data.transistor_count)
 
 var changeColor = function(){
   svg.selectAll("rect")
