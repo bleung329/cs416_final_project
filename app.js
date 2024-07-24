d570675719f0b7853852b2953ea624633a0cf015
Brian Leung <brianleung329@gmail.com>
2024-07-24 01:18:09 -0400

    Zooming and misc works now.

--- app.js
+++ app.js
@@ -1,37 +1,46 @@
+//Display moores law line chart
+
+//Change scale of x axis (Filtering year)
+//Change scale of y axis (log,linear,points)
+
+//Filter out points (Select company)
+//Mouseover to view annotation
+
 //<<< Helper Function Definitions >>>
 
-function make_scatter(data,start_year,end_year)
+function init_scatter(data)
 {
-  var filtered = data.filter(function(d){ return d.year >= start_year && d.year <= end_year})
+  // create a clipping region
+  svg.append("defs").append("clipPath")
+  .attr("id", "clip")
+  .append("rect")
+  .attr("width", width+5)
+  .attr("height", height+5);
 
-  svg
+  scatter = svg
+  .append('g')
+  .attr("clip-path","url(#clip)")
   .selectAll("circle")
-  .data(filtered)
+  .data(data)
   .enter()
   .append("circle")
   .attr("cx", function(d,i){ return yearScale(d.year)})
   .attr("cy", function(d,i){ return transistorScale(d.transistor_count);})
-  .attr("r", 5)
+  .attr("r", 4)
   .attr("fill", function(d){
     switch (d.designer) {
       case "AMD":
         return "red"
-        break;
       case "Intel":
         return "blue"
-        break;
       case "Apple":
         return "black"
-        break;
       case "IBM":
         return "#ffb6c1"
-        break;
       default:
         return "#d3d3d3"
-        break;
     }
-  });
-
+  })
 }
 
 //<<< Main >>>
@@ -47,10 +56,9 @@ const svg = d3
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+          "translate(" + margin.left + "," + margin.top + ")")
 
-yearScale = null;
-transistorScale = null;
+var yearScale,yearAxis,transistorScale,transistorAxis,scatter = null;
 
 const all_data = d3.csv("https://raw.githubusercontent.com/bleung329/cs416_final_project/main/data/processors.csv")
 .then(
@@ -58,26 +66,73 @@ const all_data = d3.csv("https://raw.githubusercontent.com/bleung329/cs416_final
 
     // X axis
     yearScale = d3.scaleLinear()
-      .domain(d3.extent(data, function(d) {  
-        return d.year; }))
-      .range([ 0, width ]);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(yearScale).tickFormat(d3.format("d")));
+                .domain(d3.extent(data, 
+                  function(d) {  
+                    return d.year; 
+                  }))
+                .range([ 0, width ]);
+
+    yearAxis =  svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(yearScale).tickFormat(d3.format("d")));
+    
+    svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("y", 6)
+    .attr("dy", ".75em")
+    .attr("transform", "rotate(-90)")
+    .text("TRANSISTOR COUNT");
   
     // Y axis
     transistorScale = d3.scaleLog()
-      .domain([1, d3.max(data, function(d) { return +d.transistor_count; })])
-      .range([ height, 0 ]);
-    svg.append("g")
-      .call(d3.axisLeft(transistorScale));
+                      .domain([1, d3.max(data, 
+                        function(d) { 
+                          return +d.transistor_count; 
+                        })])
+                      .range([ height, 0 ]);
 
-    make_scatter(data,1970,2024)
-    make_scatter(data,1970,2024)
+    transistorAxis =  svg.append("g")
+                      .call(d3.axisLeft(transistorScale));
+
+    svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("x", width)
+    .attr("y", height - 6)
+    .text("YEAR");
+
+    init_scatter(data)
+    
+    var zoom =  d3.zoom()
+    .scaleExtent([1, 5])
+    .translateExtent([[0, 0], [width, height]])
+    .extent([[0, 0], [width, height]])
+    .on("zoom", updateChart);
+    
+    svg.append("rect")
+    .attr("width", width)
+    .attr("height", height)
+    .style("fill", "none")
+    .style("pointer-events", "all")
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+    .call(zoom).call(zoom.transform, d3.zoomIdentity);
+    
+    function updateChart({transform}) {
+    
+      // recover the new scale
+      var newYearScale = transform.rescaleX(yearScale).interpolate(d3.interpolateRound);
+      var newTransistorScale = transform.rescaleY(transistorScale).interpolate(d3.interpolateRound);
+    
+      // update axes with these new boundaries
+      yearAxis.call(d3.axisBottom(newYearScale).tickFormat(d3.format("d")))
+      transistorAxis.call(d3.axisLeft(newTransistorScale))
+    
+      // update circle positions
+      scatter.attr("transform",transform)
+    }
+
     console.log("DONE?")
   }
 )
-
 
 var changeColor = function(){
   svg.selectAll("circle")
